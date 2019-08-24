b6c344271c25fe24e58b14e1b91743aa4f147de7
shivanshu gupta <shivanshu.gupta4@gmail.com>
2019-08-24 20:11:52 +0530

    Add files via upload

---
+++ AddSurveyViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurveyApplication.Models
+{
+    public class AddSurveyViewModel
+    {
+        public int Survey_ID { get; set; }
+        public string Survey_Type { get; set; }
+
+    }
+
+}
